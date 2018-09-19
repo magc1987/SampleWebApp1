3eb6032be95f2699441f2751adaba5d9308e604f
Marvyn Chan <marvyn_d_martian@outlook.com>
2018-09-20 00:46:03 +0800

    updating

--- SampleWebApp1/Pages/Index.cshtml.cs
+++ SampleWebApp1/Pages/Index.cshtml.cs
@@ -12,12 +12,33 @@ namespace SampleWebApp1.Pages
     {
 
         [BindProperty]
-        public Person Person { get; set;
-        }
+        public Person Person { get; set;}
+
+        [BindProperty(SupportsGet =true)]
+        public IList<Person> Persons { get; set; }
 
         public void OnGet()
         {
-            
+            Person[] p = new Person[]
+            {
+                new Person{FirstName="Butch", LastName="Roque"},
+                new Person{ FirstName="Marv", LastName="Chan"},
+                new Person{FirstName="Jonas", LastName="Bico"},
+                new Person{FirstName="Lennie", LastName="Chua"},
+                new Person{FirstName="Alymer", LastName="Hernandez"},
+                new Person{FirstName="Joseph", LastName="Ang"},
+            };
+            Persons = p;
+            foreach (var item in Persons)
+            {
+                
+            }
+        }
+
+        public void OnPostAsync()
+        {
+            IList<Person> p = Persons;
+
         }
     }
 }
